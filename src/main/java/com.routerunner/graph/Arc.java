984d95a7b819b94052c07820d0634cb11bcc3a5c
Mehrdad <moxitron99@gmail.com>
2023-08-02 17:27:00 +0330

    feat: refactore graph

---
+++ src/main/java/com.routerunner/graph/Arc.java
@@ -0,0 +1,24 @@
+package main.java.com.routerunner.graph;
+
+/**
+ * Arc is used as an edge in road network graph.
+ * cost corresponds to edge weight and headNodeId is id of the node
+ * the arc is pointing to. for example in A -------> B, B is headNode.
+ */
+public class Arc {
+    int headNodeId;
+    int cost;
+
+    public Arc(int headNodeId, int cost) {
+        this.headNodeId = headNodeId;
+        this.cost = cost;
+    }
+
+    public int getHeadNodeId() {
+        return headNodeId;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}

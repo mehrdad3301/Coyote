03106bdaf46c439aae98fc483af555a1c75ef855
arsalanaa44 <mehran.mah44@gmail.com>
2023-08-16 12:17:20 +0330

    fix: fix random generator

--- core/src/main/java/com.routerunner/util/Random.java
+++ core/src/main/java/com.routerunner/util/Random.java
@@ -6,17 +6,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Random {
 
+    /**
+     * this can be made more efficient easily, but when numPoints
+     * are limited, this is good enough
+     * @param numPoints number of random numbers to be generated
+     * @return returns numPoints distinct random numbers
+     */
     public static ArrayList<Integer> generateRandom(int numPoints, int lowerBound, int upperBound) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = lowerBound ; i <= upperBound; i++) {
-            numbers.add(i);
+        ArrayList<Integer> randomNum = new ArrayList<>(numPoints);
+        java.util.Random rand = new java.util.Random();
+        for (int i = 0; i < numPoints; i++) {
+            int num;
+            do {
+                num = rand.nextInt((upperBound - lowerBound) + 1) + lowerBound;
+            } while (randomNum.contains(num));
+            randomNum.add(num);
         }
-        Collections.shuffle(numbers);
-        ArrayList<Integer> randomNum = new ArrayList<>(numbers.subList(0, numPoints));
         return randomNum ;
     }
 
     public static int getPositiveRandom(int max) {
         return ThreadLocalRandom.current().nextInt(0, max);
     }
+
 }

fa384bccd0b088168ed72cf4d217ab758216dffa
Mehrdad <moxitron99@gmail.com>
2023-08-17 13:22:31 +0330

    test: test new functionality in dijkstra

--- core/src/test/java/com/routerunner/graph/TestAlgorithm.java
+++ core/src/test/java/com/routerunner/graph/TestAlgorithm.java
@@ -5,6 +5,7 @@ import com.routerunner.algorithms.ATL;
 import com.routerunner.algorithms.Dijkstra;
 import org.junit.Test;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
@@ -28,24 +29,24 @@ public class TestAlgorithm {
 
         int cost = dij.computeShortestPath(4, 3) ;
         assertEquals(dij.getPath(3).toString(), "Path 104 - 324 > 101 - 528 > 103 ");
-        assertEquals(dij.getNumVisitedNodes(), 5);
+        assertEquals(dij.getNumSettledNodes(), 5);
 
         cost = dij.computeShortestPath(0, 6) ;
         assertEquals(dij.getPath(6).toString(), "Path 100 - 485 > 104 - 616 > 102 - 536 > 106 ");
         assertEquals(cost, 1637);
-        assertEquals(dij.getNumVisitedNodes(), 7);
+        assertEquals(dij.getNumSettledNodes(), 7);
 
         graph = Graph.buildFromOSM("./src/test/resources/graph_test_3.osm");
         dij = new Dijkstra(graph);
         cost = dij.computeShortestPath(0, 7) ;
         assertEquals(dij.getPath(7).toString(), "Path 101 - 61211 > 113 - 19731 > 112 - 9765 > 111 - 15043 > 109 - 31120 > 108 ");
         assertEquals(cost,136870);
-        assertEquals(dij.getNumVisitedNodes(), 13);
+        assertEquals(dij.getNumSettledNodes(), 13);
 
         cost = dij.computeShortestPath(3, 10) ;
         assertEquals(dij.getPath(10).toString(), "Path 104 - 16118 > 102 - 14509 > 101 - 61211 > 113 - 19731 > 112 - 9765 > 111 ") ;
         assertEquals(cost, 121334);
-        assertEquals(dij.getNumVisitedNodes(), 10);
+        assertEquals(dij.getNumSettledNodes(), 10);
     }
 
     @Test
@@ -56,12 +57,12 @@ public class TestAlgorithm {
         int cost = dij.computeShortestPath(0, 11) ;
         assertEquals(dij.getPath(11).toString(), "Path 101 - 61211 > 113 - 19731 > 112 ") ;
         assertEquals(cost,80942);
-        assertEquals(dij.getNumVisitedNodes(),8);
+        assertEquals(dij.getNumSettledNodes(),8);
 
         cost = dij.computeShortestPath(6, 11) ;
         assertEquals(dij.getPath(11).toString(), "Path 107 - 60190 > 108 - 31120 > 109 - 15043 > 111 - 9765 > 112 ") ;
         assertEquals(cost, 116118);
-        assertEquals(dij.getNumVisitedNodes(),8);
+        assertEquals(dij.getNumSettledNodes(),8);
     }
     @Test
     public void TestATL() throws Exception {
@@ -81,6 +82,57 @@ public class TestAlgorithm {
         cost = atl.computeShortestPath(2, 8) ;
         assertEquals(atl.getPath(8).toString(), "Path 103 - 14815 > 101 - 61211 > 113 - 19731 > 112 - 9765 > 111 - 15043 > 109 ") ;
     }
+
+    @Test
+    public void maxSettled() {
+        Graph graph = buildSampleGraph() ;
+        Dijkstra dij = new Dijkstra(graph);
+        dij.setMaxSettledNodes(5);
+        dij.computeShortestPath(0, 7) ;
+        assertEquals(dij.getSettledIds().size(), 5) ;
+    }
+
+    @Test
+    public void maxCost() {
+        Graph graph = buildSampleGraph() ;
+        Dijkstra dij = new Dijkstra(graph);
+        dij.setCostUpperBound(20);
+        dij.computeShortestPath(0, 7) ;
+        assertEquals(dij.getSettledIds().size(), 5) ;
+    }
+
+    @Test
+    public void TestArcFlags() {
+        Graph graph = buildSampleGraph() ;
+        Dijkstra dij = new Dijkstra(graph);
+        graph.setAdjacentArcFlags(11, false);
+        dij.setUseArcFlags(true);
+        int cost = dij.computeShortestPath(0, 7) ;
+        assertEquals(cost, 64);
+        assertEquals(dij.getPath(7).toString(), "Path 0 - 4 > 2 - 6 > 3 - 7 > 4 - 7 > 5 - 20 > 6 - 20 > 7 ");
+
+        dij.setUseArcFlags(false);
+        cost = dij.computeShortestPath(0, 7) ;
+        assertEquals(cost, 60);
+        assertEquals(dij.getPath(7).toString(), "Path 0 - 30 > 11 - 15 > 9 - 5 > 8 - 10 > 7 ") ;
+
+        graph.setAdjacentArcFlags(11, true);
+        dij.setUseArcFlags(true);
+
+        cost = dij.computeShortestPath(0, 7) ;
+        assertEquals(cost, 60);
+        assertEquals(dij.getPath(7).toString(), "Path 0 - 30 > 11 - 15 > 9 - 5 > 8 - 10 > 7 ") ;
+
+    }
+
+    @Test
+    public void contractNode() {
+
+    }
+    @Test
+    public void contractNodes() {
+    }
+
 
     @Test
     public void TestLargestConnectedComponent() throws Exception {
@@ -156,6 +208,42 @@ public class TestAlgorithm {
                 		0:823
                                 
                 }""") ;
+    }
+
+    //this is similar to graph_test_3.osm, but easier to test without haversine costs
+    public Graph buildSampleGraph() {
+
+        Graph graph = new Graph() ;
+        graph.addNode(new Node(0, 10, 10)) ;
+        graph.addNode(new Node(1, 10, 10)) ;
+        graph.addNode(new Node(2, 10, 10)) ;
+        graph.addNode(new Node(3, 10, 10)) ;
+        graph.addNode(new Node(4, 10, 10)) ;
+        graph.addNode(new Node(5, 10, 10)) ;
+        graph.addNode(new Node(6, 10, 10)) ;
+        graph.addNode(new Node(7, 10, 10)) ;
+        graph.addNode(new Node(8, 10, 10)) ;
+        graph.addNode(new Node(9, 10, 10)) ;
+        graph.addNode(new Node(10, 10, 10)) ;
+        graph.addNode(new Node(11, 10, 10)) ;
+        graph.addEdge(0, 1, 3) ;
+        graph.addEdge(0, 2, 4) ;
+        graph.addEdge(0, 11, 30) ;
+        graph.addEdge(1, 2, 5) ;
+        graph.addEdge(1, 3, 10) ;
+        graph.addEdge(2, 3, 6) ;
+        graph.addEdge(3, 5, 20) ;
+        graph.addEdge(3, 4, 7) ;
+        graph.addEdge(4, 5, 7) ;
+        graph.addEdge(5, 6, 20) ;
+        graph.addEdge(6, 7, 20) ;
+        graph.addEdge(8, 7, 10) ;
+        graph.addEdge(9, 8, 5) ;
+        graph.addEdge(10, 8, 4) ;
+        graph.addEdge(11, 9, 15) ;
+        graph.addEdge(11, 10, 20) ;
+
+        return graph ;
     }
 }
 
--- core/src/test/java/com/routerunner/graph/TestUtil.java
+++ core/src/test/java/com/routerunner/graph/TestUtil.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static com.routerunner.geo.GeoMath.getHaversineDistance;
 import static org.junit.Assert.assertEquals;
 
-public class TestMathGeo {
+public class TestUtil {
 
     @Test
     public void TestHaversineDistance() {

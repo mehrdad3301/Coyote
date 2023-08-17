2201173f25133e755b13102f31885ea38770d641
Mehrdad <moxitron99@gmail.com>
2023-08-17 13:22:55 +0330

    feat: add permutation to random

--- core/src/main/java/com.routerunner/util/Random.java
+++ core/src/main/java/com.routerunner/util/Random.java
@@ -29,4 +29,14 @@ public class Random {
         return ThreadLocalRandom.current().nextInt(0, max);
     }
 
+    public static ArrayList<Integer> permutation(int n) {
+        ArrayList<Integer> numbers = new ArrayList<>(n) ;
+
+        for (int i = 0 ; i < n ; i++)
+            numbers.add(i) ;
+
+        Collections.shuffle(numbers);
+        return numbers ;
+    }
+
 }
